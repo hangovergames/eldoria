5d674fde044e5df9c165a70d5bdb2837105ab1f9
Jaakko Heusala <jhh@hg.fi>
2024-02-20 03:09:41 +0200

    Added some OpenAI generated assets

---
+++ assets/textures/tiles/eldoria.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="eldoria" tilewidth="1024" tileheight="1024" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1024" height="1024" source="../seemless-tiles/eldoria-texture-sand.png"/>
+ </tile>
+ <tile id="1">
+  <image width="1024" height="1024" source="../seemless-tiles/eldoria-texture-shallow-ocean.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1024" height="1024" source="../seemless-tiles/eldoria-texture-plains.png"/>
+ </tile>
+</tileset>
